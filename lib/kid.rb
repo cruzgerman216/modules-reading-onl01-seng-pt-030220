4df6a792e908d8e0ade6ecd136b3285b59db3fd8
German Cruz <cruzgerman216@yahoo.com>
2020-03-18 05:15:16 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,8 +3,8 @@ require_relative './class_methods_module.rb'
 require_relative './fance_dance.rb'
 
 class Kid
-  include Dance
-  attr_accessor :name
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 
   def initialize(name)
     @name = name
